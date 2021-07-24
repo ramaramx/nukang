4e988a2ffebaf80bfa6c088bed4d18bb27911f37
ramaramx <67283838+ramaramx@users.noreply.github.com>
2021-07-24 22:52:08 +0700

    Add files via upload

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/HeroSection';
+
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+        </>
+    )
+}
+
+export default Home
